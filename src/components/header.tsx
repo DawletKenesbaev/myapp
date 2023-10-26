1c4fe07e13a5e5e397ccf1ef2c44b8d913bb8c16
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-10-26 11:00:41 +0500

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import SearchInput from "./searchInput"
+
+function Header() {
+  return (
+    <>
+      <h1>Header</h1>
+      <SearchInput />
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/searchInput.tsx
@@ -0,0 +1,10 @@
+
+function SearchInput() {
+  return (
+    <>
+      <h1>search</h1>
+    </>
+  )
+}
+
+export default SearchInput;
